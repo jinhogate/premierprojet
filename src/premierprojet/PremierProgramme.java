75a37e8068115000103949edec50a746d86722a1
Gbegnon kossi jorjinho <kossijorjinho.gbegnon@gmail.com>
2023-07-26 17:12:41 +0200

    Created main program

---
+++ src/premierprojet/PremierProgramme.java
@@ -0,0 +1,26 @@
+package premierprojet;
+
+/**
+ * Premiere classe pour tester l'installation
+ *
+ * @author JINHOGATE
+ *
+ */
+public class PremierProgramme {
+//	static char maVariableChar;
+//	static byte maVariableByte;
+//	static double maVariableDouble;
+//	static String maVariableString;
+
+	public static void main(String[] args) {
+//		System.out.println("Bonjour tout le monde!");
+//		byte minByte = -128;
+//		byte maxByte = 127;
+//		System.out.println(maVariableChar);
+//		System.out.println(maVariableByte);
+//		System.out.println(maVariableDouble);
+//		System.out.println(maVariableString);
+
+	}
+
+}
